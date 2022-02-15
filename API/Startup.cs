e21b6309407f03950fc6315c4505ed02897656e3
Rayvax <Rayvax213@gmail.com>
2022-02-15 08:49:00 +0300

    CSP bug fix

--- API/Startup.cs
+++ API/Startup.cs
@@ -54,12 +54,12 @@ namespace API
                 .FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com", "data:"))
                 .FormActions(s => s.Self())
                 .FrameAncestors(s => s.Self())
-                .ImageSources(s => s.Self().CustomSources("https://res.cloudinary.com", "blob:", 
+                .ImageSources(s => s.Self().CustomSources("https://res.cloudinary.com", "blob:",
                     "data:", "https://sun9-24.userapi.com"))
-                .ScriptSources(s => s.Self().CustomSources("sha256-5z2+Ze10iTQeEhl5yYkIAwkHn3wDBubYTTCXC0g4QB0="))
-            
-                );
-            
+                .ScriptSources(s => s.Self().CustomSources("sha256-5z2+Ze10iTQeEhl5yYkIAwkHn3wDBubYTTCXC0g4QB0=",
+                    "sha256-kXwZFeDqzQYQxMANlJcsdedkJvek1q5ncjzFrCq4x+I="))
+            );  
+
             if (env.IsDevelopment())
             {
                 app.UseSwagger();
